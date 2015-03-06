92b624790831b7b6c013035e3bb8101f408720a0
José F. Romaniello <jfromaniello@gmail.com>
2015-03-06 09:42:25 -0300

    add support for decoding utf8 tokens

--- test/tests.js
+++ test/tests.js
@@ -9,4 +9,15 @@ describe('Auth0', function () {
     expect(decoded.foo).to.equal('bar');
   });
 
+  it('should work with utf8 tokens', function () {
+    var utf8_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiSm9zw6kiLCJpYXQiOjE0MjU2NDQ5NjZ9.1CfFtdGUPs6q8kT3OGQSVlhEMdbuX0HfNSqum0023a0";
+    var decoded = jwt_decode(utf8_token);
+    expect(decoded.name).to.equal('José');
+  });
+
+  it('should work with binary tokens', function () {
+    var binary_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiSm9z6SIsImlhdCI6MTQyNTY0NDk2Nn0.cpnplCBxiw7Xqz5thkqs4Mo_dymvztnI0CI4BN0d1t8";
+    var decoded = jwt_decode(binary_token);
+    expect(decoded.name).to.equal('José');
+  });
 });
