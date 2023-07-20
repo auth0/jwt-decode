7c448de43e9126f79d5d8b8efa41243c366e053d
frederikprijck <frederik.prijck@auth0.com>
2023-07-20 15:42:48 +0200

    fix: fix jest tests for ESM

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,22 @@
-module.exports = {
-    rootDir: './',
-    moduleFileExtensions: ['ts', 'js'],
-    testMatch: ['**/test/**/*.test.ts', '**/test/tests.ts'],
-    coverageProvider: 'v8',
-    coveragePathIgnorePatterns: [
-      '/node_modules/',
-      './jest.config.js',
-      './test',
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  rootDir: "./",
+  moduleFileExtensions: ["ts", "js"],
+  testMatch: ["**/test/**/*.test.ts", "**/test/tests.ts"],
+  coverageProvider: "v8",
+  coveragePathIgnorePatterns: ["/node_modules/", "./jest.config.js", "./test"],
+  extensionsToTreatAsEsm: ['.ts'],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "test-results/jest" }],
+  ],
+  coverageReporters: ["lcov", "text", "text-summary"],
+  setupFiles: [],
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      { useESM: true, tsconfig: "./tsconfig.test.json" },
     ],
-    reporters: [
-      'default',
-      ['jest-junit', { outputDirectory: 'test-results/jest' }]
-    ],
-    coverageReporters: ['lcov', 'text', 'text-summary'],
-    setupFiles: [],
-    preset: 'ts-jest',
-    transform: {
-      '^.+\\.ts?$': ['ts-jest', {tsconfig: './tsconfig.test.json'}]
-    },
-  };
+  },
+};
