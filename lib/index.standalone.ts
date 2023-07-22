56586fde044673f90c58bf1142b10abe604c8469
frederikprijck <frederik.prijck@auth0.com>
2023-07-22 22:41:12 +0200

    feat: cleanup UMD entry file

--- lib/index.standalone.ts
+++ lib/index.standalone.ts
@@ -1,16 +1,3 @@
-/*
- * Expose the function on the window object
- */
 import jwtDecode from "./index";
 
-//use amd or just through the window object.
-const global = window as any;
-if (global) {
-    if (typeof global.define == "function" && global.define.amd) {
-        global.define("jwt_decode", function() {
-            return jwtDecode;
-        });
-    } else if (global) {
-        global.jwt_decode = jwtDecode;
-    }
-}
+export default jwtDecode;
