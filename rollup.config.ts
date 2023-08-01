307f3de57cd322df1983c1876d2bf3c9431574ef
Jon Koops <jonkoops@gmail.com>
2023-08-01 12:38:09 +0200

    Stop minifying ESM and CommonJS for distribution

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,40 +1,27 @@
-import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 import livereload from "rollup-plugin-livereload";
 import serve from "rollup-plugin-serve";
 
 const isProduction = process.env.NODE_ENV === "production";
-const tsPlugin = typescript({
-  rootDir: "lib",
-  sourceMap: true,
-});
-
-const plugins = [
-  tsPlugin,
-  isProduction && terser(),
-];
-
+const plugins = [typescript({ rootDir: "lib" })];
 const input = "lib/index.ts";
 
-export default defineConfig([{
+export default defineConfig([
+  {
     input: "lib/index.umd.ts",
     output: {
       name: "jwt_decode",
       file: "build/jwt-decode.js",
       format: "umd",
-      sourcemap: true,
     },
-    plugins: [
-      tsPlugin,
-    ]
+    plugins,
   },
   {
     input,
     output: {
       file: "build/cjs/jwt-decode.cjs",
       format: "cjs",
-      sourcemap: true,
     },
     plugins,
   },
@@ -43,14 +30,15 @@ export default defineConfig([{
     output: {
       file: "build/esm/jwt-decode.js",
       format: "esm",
-      sourcemap: true,
     },
-    plugins: [!isProduction &&
-      serve({
-        contentBase: ["build", "static"],
-        open: true,
-        port: 3000,
-      }), !isProduction && livereload(),
+    plugins: [
+      !isProduction &&
+        serve({
+          contentBase: ["build", "static"],
+          open: true,
+          port: 3000,
+        }),
+      !isProduction && livereload(),
       ...plugins,
     ],
     watch: {
